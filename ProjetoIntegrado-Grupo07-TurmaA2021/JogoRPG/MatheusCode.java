26dda8a2a47f269ac13a4fd672ce689b1a7e6fa9
mathrochaa <54423058+mathrochaa@users.noreply.github.com>
2021-05-16 19:09:47 -0300

    Update MatheusCode.java
    alterei a função questaoMultiplaEscolha

--- ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
+++ ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
@@ -6,20 +6,22 @@ import java.util.Scanner;
 public class MatheusCode {
 	public static Scanner leitor = new Scanner(System.in);
 
-	public static boolean questaoMultiplaEscolha(String pergunta, ArrayList<String> alternativas,
+	public static float questaoMultiplaEscolha(String pergunta, ArrayList<String> alternativas,
 			String alternativaCorreta) {
 		String resposta;
-		boolean acertou = false;
+		float acertou = 0;
 
-		String perguntaConsolidada = pergunta + "\n\n" + alternativas.get(0) + "\n" + alternativas.get(1) + "\n"
-				+ alternativas.get(2) + "\n" + alternativas.get(3) + "\n" + alternativas.get(4);
-		System.out.println(perguntaConsolidada);
+		System.out.println(pergunta);
+		for (String alternativa : alternativas) {
+			System.out.println(alternativa);
+		}
+
 		resposta = leitor.next();
 		resposta = resposta.toLowerCase();
 
 		if (resposta.equals(alternativaCorreta)) {
 			System.out.println("Alternativa correta");
-			acertou = true;
+			acertou = 1;
 		} else {
 			System.out.println("Alternativa errada");
 		}
@@ -28,27 +30,57 @@ public class MatheusCode {
 
 	public static void main(String[] args) {
 
-		int pontuacao = 0;
-		String a, b, c, d, e;
+		float pontuacao = 0;
+		String a, b, c, d, e, pergunta;
+		ArrayList<String> alternativas = new ArrayList<String>();
 
+		pergunta = "Qual valor hexadecimal que convertido para octagonal fica igual a 247?";
 		a = "a) A7";
 		b = "b) A4";
 		c = "c) A3";
 		d = "d) B1";
 		e = "e) BF";
-		ArrayList<String> alternativas = new ArrayList<String>();
-
 		alternativas.add(a);
 		alternativas.add(b);
 		alternativas.add(c);
 		alternativas.add(d);
 		alternativas.add(e);
 
-		if (questaoMultiplaEscolha("Qual valor hexadecimal que convertido para octagonal fica igual a 247?",
-				alternativas, "a") == true)
-			pontuacao++;
+		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "a");
 
-		System.out.printf("Você teve um aproveitamento de %d%% no teste", ((pontuacao / 1) * 100));
+		alternativas.clear();
+
+		pergunta = "Qual valor decimal que convertido para binario fica igual a 0100?";
+		a = "a) 1";
+		b = "b) 2";
+		c = "c) 3";
+		d = "d) 4";
+		e = "e) 5";
+		alternativas.add(a);
+		alternativas.add(b);
+		alternativas.add(c);
+		alternativas.add(d);
+		alternativas.add(e);
+
+		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "d");
+
+		alternativas.clear();
+
+		pergunta = "Qual valor decimal que convertido para binario fica igual a 15?";
+		a = "a) 0101";
+		b = "b) 1111";
+		c = "c) 1000";
+		d = "d) 0100";
+		e = "e) 1110";
+		alternativas.add(a);
+		alternativas.add(b);
+		alternativas.add(c);
+		alternativas.add(d);
+		alternativas.add(e);
+
+		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "b");
+
+		System.out.printf("Você teve um aproveitamento de %.0f%% no teste", ((pontuacao / 3) * 100));
 		leitor.close();
 
 	}
