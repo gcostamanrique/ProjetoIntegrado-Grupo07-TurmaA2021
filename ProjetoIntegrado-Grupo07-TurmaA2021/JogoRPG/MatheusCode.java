6b6e780618d64353e89d02ab9198e170cbd95b3e
mathrochaa <54423058+mathrochaa@users.noreply.github.com>
2021-05-17 15:39:55 -0300

    Update MatheusCode.java

--- ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
+++ ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
@@ -8,34 +8,57 @@ import java.util.Scanner;
 public class MatheusCode {
 	public static Scanner leitor = new Scanner(System.in);
 
-	public static float questaoMultiplaEscolha(String pergunta, ArrayList<String> alternativas,
+	public static float questaoMultiplaEscolha(String pergunta, ArrayList<String> listaAlternativas,
 			String alternativaCorreta) {
 		String resposta;
 		float acertou = 0;
+		int posicaoAlternativa;
+		char letraAlternativa;
+		ArrayList<String> listaLetrasAlternativas = new ArrayList<String>();
 
-		Collections.shuffle(alternativas);
-		Collections.shuffle(alternativas);
-		Collections.shuffle(alternativas);
+		Collections.shuffle(listaAlternativas);
+		Collections.shuffle(listaAlternativas);
+		Collections.shuffle(listaAlternativas);
 
-		System.out.println(pergunta);
-		for (int i = 0; i < alternativas.size(); i++) {
-			System.out.println("[ " + (char) (i + 65) + " ] " + alternativas.get(i));
+		do {
+			System.out.println(pergunta);
 
-		}
+			for (int i = 0; i < listaAlternativas.size(); i++) {
 
-		resposta = leitor.next();
-		resposta = resposta.toUpperCase();
+				listaLetrasAlternativas.add(Character.toString((char) (i + 65))); // interando a lista de alternativas
+				System.out.println("[ " + listaLetrasAlternativas.get(i) + " ] " + listaAlternativas.get(i)); // apresentando
+																												// elas
+																												// junto
+																												// com
+																												// as
+																												// questões
+			}
 
-		int posicaoAlternativa = (alternativas.indexOf(alternativaCorreta));
-		char letraAlternativa = ((char) (posicaoAlternativa + 65));
+			resposta = leitor.next().toUpperCase();
 
-		if ((resposta.equals(alternativaCorreta)) || resposta.equals(String.valueOf(letraAlternativa))) {
+			posicaoAlternativa = (listaAlternativas.indexOf(alternativaCorreta));
+			letraAlternativa = ((char) (posicaoAlternativa + 65));
+
+		} while (naoPertence(resposta, listaLetrasAlternativas)); // enquanto ele não pertencer a lista de
+																	// alternativas ele não vai sair do looping
+
+		if (resposta.equals(String.valueOf(letraAlternativa))) {
 			System.out.println("Alternativa correta");
 			acertou = 1;
 		} else {
 			System.out.println("Alternativa errada");
 		}
+
 		return acertou;
+	}
+
+	public static boolean naoPertence(String valor, ArrayList<String> vetor) {
+		for (String item : vetor) {
+			if (item.equals(valor)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
@@ -46,7 +69,7 @@ public class MatheusCode {
 
 		pergunta = "Qual valor hexadecimal que convertido para octagonal fica igual a 247?";
 		alternativas.addAll(Arrays.asList("A7", "A4", "A3", "B1", "BF"));
-		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "A7");
+		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, alternativas.get(0));
 		alternativas.clear();
 
 		pergunta = "Qual valor decimal que convertido para binario fica igual a 0100?";
@@ -54,7 +77,12 @@ public class MatheusCode {
 		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "4");
 		alternativas.clear();
 
-		System.out.printf("Você acertou %.0f no teste", pontuacao);
+		pergunta = "Qual valor decimal que convertido para binario fica igual a 1000?";
+		alternativas.addAll(Arrays.asList("8", "1", "3", "4"));
+		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "8");
+		alternativas.clear();
+
+		System.out.printf("Você acertou %.0f questão(ões) no teste", pontuacao);
 		leitor.close();
 
 	}
