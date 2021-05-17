8082f5cb8b626d27d314222cae695dfc86532e43
mathrochaa <54423058+mathrochaa@users.noreply.github.com>
2021-05-17 12:17:21 -0300

    Update MatheusCode.java

--- ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
+++ ProjetoIntegrado-Grupo07-TurmaA2021/JogoRPG/MatheusCode.java
@@ -19,16 +19,16 @@ public class MatheusCode {
 
 		System.out.println(pergunta);
 		for (int i = 0; i < alternativas.size(); i++) {
-			System.out.println((i + 1) + ". " + alternativas.get(i));
+			System.out.println((char)(i + 65) + ". " + alternativas.get(i));
 
 		}
 		resposta = leitor.next();
 		resposta = resposta.toUpperCase();
 
-		int posicaoAlternativa = (alternativas.indexOf(alternativaCorreta) + 1);
-		System.out.println(posicaoAlternativa);
-
-		if ((resposta.equals(alternativaCorreta)) || resposta.equals(Integer.toString(posicaoAlternativa))) {
+		int posicaoAlternativa = (alternativas.indexOf(alternativaCorreta));
+		char letraAlternativa = ((char)(posicaoAlternativa + 65));
+		
+		if ((resposta.equals(alternativaCorreta)) || resposta.equals(String.valueOf(letraAlternativa))) {
 			System.out.println("Alternativa correta");
 			acertou = 1;
 		} else {
@@ -40,38 +40,20 @@ public class MatheusCode {
 	public static void main(String[] args) {
 
 		float pontuacao = 0;
-		String a, b, c, d, e, pergunta;
+		String pergunta;
 		ArrayList<String> alternativas = new ArrayList<String>();
 
 		pergunta = "Qual valor hexadecimal que convertido para octagonal fica igual a 247?";
-
 		alternativas.addAll(Arrays.asList("A7", "A4", "A3", "B1", "BF"));
-
 		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "A7");
-
 		alternativas.clear();
+		
 		pergunta = "Qual valor decimal que convertido para binario fica igual a 0100?";
 		alternativas.addAll(Arrays.asList("1", "2", "3", "4", "5"));
-
 		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "4");
-
 		alternativas.clear();
 
-		pergunta = "Qual valor decimal que convertido para binario fica igual a 15?";
-		a = "a) 0101";
-		b = "b) 1111";
-		c = "c) 1000";
-		d = "d) 0100";
-		e = "e) 1110";
-		alternativas.add(a);
-		alternativas.add(b);
-		alternativas.add(c);
-		alternativas.add(d);
-		alternativas.add(e);
-
-		pontuacao = pontuacao + questaoMultiplaEscolha(pergunta, alternativas, "1111");
-
-		System.out.printf("Você teve um aproveitamento de %.0f%% no teste", ((pontuacao / 3) * 100));
+				System.out.printf("Você teve um aproveitamento de %.0f%% no teste", ((pontuacao / 3) * 100));
 		leitor.close();
 
 	}
